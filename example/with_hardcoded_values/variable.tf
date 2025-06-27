49f8b81267fdea7908b68d8471658944b4786901
jnikita19 <Nikita.Joshi.snaatak@mygurukulam.co>
2025-06-27 07:04:13 +0530

    add default values in module variable.tf

--- example/with_hardcoded_values/variable.tf
+++ example/with_hardcoded_values/variable.tf
@@ -1,31 +1,37 @@
 variable "requester_vpc_id" {
   description = "The VPC ID of the requester VPC"
   type        = string
+  default = "vpc-0d76a177b1c5df301"
 }
 
 variable "acceptor_vpc_id" {
   description = "The VPC ID of the acceptor VPC"
   type        = string
+  default = "vpc-0bfa15004ff55e107"
 }
 
 variable "requester_vpc_cidr" {
   description = "The CIDR block of the requester VPC"
   type        = string
+  default = "10.1.0.0/16"
 }
 
 variable "acceptor_vpc_cidr" {
   description = "The CIDR block of the acceptor VPC"
   type        = string
+  default = "172.31.0.0/16"
 }
 
 variable "requester_route_table_ids" {
   description = "List of route table IDs associated with the requester VPC"
   type        = list(string)
+  default = [ "rtb-0bba85668091331a0" ]
 }
 
 variable "acceptor_route_table_ids" {
   description = "List of route table IDs associated with the acceptor VPC"
   type        = list(string)
+  default = [ "rtb-0f05d6fd762bf3a45" ]
 }
 
 variable "requester_region" {
--- variable.tf
+++ variable.tf
@@ -5,26 +5,31 @@
 variable "bu" {
   description = "Business Unit name (e.g., finance, marketing)"
   type        = string
+  default = "ot"
 }
 
 variable "program" {
   description = "Program or department name"
   type        = string
+  default = "ot-cloud-kit"
 }
 
 variable "app" {
   description = "Application name"
   type        = string
+  default = "bp"
 }
 
 variable "env" {
   description = "Deployment environment (e.g., dev, prod, staging)"
   type        = string
+  default = "d"
 }
 
 variable "team" {
   description = "Team responsible for the resources"
   type        = string
+  default = "infra"
 }
 
 # --------------------------------------
@@ -34,31 +39,37 @@ variable "team" {
 variable "requester_vpc_id" {
   description = "VPC ID of the requester"
   type        = string
+  default = "vpc-0d76a177b1c5df301"
 }
 
 variable "acceptor_vpc_id" {
   description = "VPC ID of the acceptor"
   type        = string
+  default = "vpc-0bfa15004ff55e107"
 }
 
 variable "requester_vpc_cidr" {
   description = "CIDR block of the requester VPC"
   type        = string
+  default = "10.1.0.0/16"
 }
 
 variable "acceptor_vpc_cidr" {
   description = "CIDR block of the acceptor VPC"
   type        = string
+  default = "172.31.0.0/16"
 }
 
 variable "requester_route_table_ids" {
   description = "List of route table IDs for requester VPC"
   type        = list(string)
+  default = [ "rtb-0bba85668091331a0" ]
 }
 
 variable "acceptor_route_table_ids" {
   description = "List of route table IDs for acceptor VPC"
   type        = list(string)
+  default = [ "rtb-0f05d6fd762bf3a45" ]
 }
 
 # --------------------------------------
